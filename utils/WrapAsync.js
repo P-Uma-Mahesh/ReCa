f2459942370e1662f46adfa964db87128efe69eb
P-Uma-Mahesh <159941265+P-Uma-Mahesh@users.noreply.github.com>
2025-07-18 15:18:07 +0530

    Update WrapAsync.js

--- utils/WrapAsync.js
+++ utils/WrapAsync.js
@@ -1,7 +1,7 @@
+// to avoid to write try and catch in every functions
+// use this function to every route in app 
 module.exports=(fn)=>{
     return (req,res,next)=>{
         fn(req,res,next).catch(next);
     }
 }
-// to avoid to write try and catch in every functions
-// use this function to every route in app 
